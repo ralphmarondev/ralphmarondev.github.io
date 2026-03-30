51f1aaf8284d8dcc3daadb5807670e460d56e212
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-03-30 12:57:58 +0800

    made the main always w-5xl on TutorialMainLayout.vue

--- src/shared/layouts/TutorialMainLayout.vue
+++ src/shared/layouts/TutorialMainLayout.vue
@@ -16,7 +16,7 @@ const props = withDefaults(defineProps<Props>(), {
 	<div class="flex flex-col min-h-screen">
 		<nav class="bg-white dark:bg-gray-900 shadow-md sticky top-0 w-full z-50">
 			<div class="max-w-5xl mx-auto">
-				<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+				<div class="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
 					<router-link :to="props.link" class="flex items-center space-x-2">
 						<i class="bx bx-code text-2xl text-gray-800 dark:text-white"></i>
 						<span class="font-bold text-xl text-gray-800 dark:text-white">
@@ -27,7 +27,7 @@ const props = withDefaults(defineProps<Props>(), {
 			</div>
 		</nav>
 
-		<main class="flex-1 px-6 py-6 md:px-8 lg:px-12 max-w-5xl mx-auto">
+		<main class="flex-1 px-6 py-6 md:px-8 lg:px-12 w-5xl mx-auto">
 			<slot />
 		</main>
 
